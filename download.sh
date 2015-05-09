d1c6fb69a5305f553159f2cac58818ad94534b90
Lucien Gaitskell <lucien@gaitskell.com>
2015-05-09 09:53:13 -0400

    Revert 5ff8ade..f1fdc67
    This rolls back to commit 5ff8ade2450360ef3c4cb92cae05522874f46e0e.

--- download.sh
+++ download.sh
@@ -3,15 +3,15 @@
 cd /var/www/Website-SST
 
 
-#if [ -d "Website-SST-master" ]; then
-#  sudo rm -rf Website-SST-master
-#fi
+if [ -d "Website-SST-master" ]; then
+  sudo rm -rf Website-SST-master
+fi
 
-#if [ -f "master.zip" ]; then
-#  sudo rm master.zip
-#fi
+if [ -f "master.zip" ]; then
+  sudo rm master.zip
+fi
 
 
 
-wget -N https://github.com/luciengaitskell/Website-SST/archive/master.zip
+wget https://github.com/luciengaitskell/Website-SST/archive/master.zip
 sudo unzip master.zip
