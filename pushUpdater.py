ffb77505002d6d6c2c060e923022ee94899e8e3e
Lucien Gaitskell <lucien@gaitskell.com>
2015-05-09 09:54:48 -0400

    Revert "Revert 5ff8ade..f1fdc67"
    This reverts commit d1c6fb69a5305f553159f2cac58818ad94534b90.

--- pushUpdater.py
+++ pushUpdater.py
@@ -1,20 +1,36 @@
 from flask import Flask, request
 import subprocess
+import os
+import sys
 
 app = Flask(__name__)
 
+
+
+
+if (sys.argv=>2):
+	branch=str(sys.argv[1])
+else:
+	branch=True
+
 @app.route("/payload", methods=['POST'])
-def cheese():
+def main():
 	theJSON = request.get_json()
-	if "master" in theJSON["ref"]:
-		#runs only if the branch commited to is the master 
-		subprocess.call("sudo pkill -f index.py", shell=True)
-		subprocess.call("cd /var/www/new_test && sudo ./download.sh", shell=True)
-		subprocess.call("sudo screen -d -m  python /var/www/Website-SST/Website-SST-master/index.py", shell=True)
+	if branch or branch in theJSON["ref"]:
+		#runs only if the branch commited to is the one supplied by the command line arg or if there isn't one
+		if (len(sys.argv)=>5):
+			webserverFile=str(sys.argv[4])
+			subprocess.call("sudo pkill -f " + str(webserverFile), shell=True)
+
+		subprocess.call("cd "
+		 + str(os.path.dirname(os.path.realpath(__file__)))
+		 + " && sudo ./download.sh"
+		, shell=True)
+
+		if (len(sys.argv)=>5):
+			subprocess.call("sudo screen -d -m  python " + str(webserverFile), shell=True)
 		return "Commit to master -- redownloading repo"
-	else:
-		return "Not committing to master -- ignoring"
-	return "derp herp this is weird"
+	return "Not committing to wanted branch -- ignoring"
 
 if __name__ == "__main__":
 	app.debug = True
@@ -22,4 +38,3 @@ if __name__ == "__main__":
 		host='0.0.0.0',
 		port=4567
 		)
-
