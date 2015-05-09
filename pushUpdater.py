7edd702c91a31da65fa90130bdde35617b6e1223
Lucien Gaitskell <lucien@gaitskell.com>
2015-05-09 09:00:12 -0400

    before change

--- pushUpdater.py
+++ pushUpdater.py
@@ -4,10 +4,10 @@ import subprocess
 app = Flask(__name__)
 
 @app.route("/payload", methods=['POST'])
-def cheese():
+def main():
 	theJSON = request.get_json()
 	if "master" in theJSON["ref"]:
-		#runs only if the branch commited to is the master 
+		#runs only if the branch commited to is the master
 		subprocess.call("sudo pkill -f index.py", shell=True)
 		subprocess.call("cd /var/www/new_test && sudo ./download.sh", shell=True)
 		subprocess.call("sudo screen -d -m  python /var/www/Website-SST/Website-SST-master/index.py", shell=True)
@@ -22,4 +22,3 @@ if __name__ == "__main__":
 		host='0.0.0.0',
 		port=4567
 		)
-
